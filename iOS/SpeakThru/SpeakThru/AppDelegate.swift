378d2efcf0d6482d343fcc749a66e210d44f997f
Dmitry Tkachenko <tkachenko.da@phystech.edu>
2019-03-09 00:02:11 +0300

    Add resources, implement launch screen

--- iOS/SpeakThru/SpeakThru/AppDelegate.swift
+++ iOS/SpeakThru/SpeakThru/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow()
-        self.window?.rootViewController = ViewController()
+        self.window?.rootViewController = MainViewController()
         self.window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
---
+++ iOS/SpeakThru/SpeakThru/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  SpeakThru
+//
+//  Created by Дмитрий Ткаченко on 08/03/2019.
+//  Copyright © 2019 klabertants. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
